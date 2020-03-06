8d1e13f5bd9cd6bac55aea8b803b92f924e4739f
Alaa M <alaagmohamed90@gmail.com>
2020-03-06 12:02:33 -0800

    practice class components in react

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, {Component} from "react";
+import JokeList from "./JokeList";
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <JokeList />
+      </div>
+    );
+  }
+}
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <JokeList />
+//     </div>
+//   );
+// }
+
+
+
+export default App;
